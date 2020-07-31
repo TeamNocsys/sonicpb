da1b2baa48d721bd5261ce649b17e45200dac103
Freddy <fwdssg@gmail.com>
2020-07-31 16:37:37 +0800

    Adjust the protobuf file generation method

--- compile_protos_java.sh
+++ compile_protos_java.sh
@@ -10,16 +10,10 @@ fi
 
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/yext.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/ywrapper.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_acl_enums.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_acl_enums.proto
+protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_acl.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_interface_enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_interface.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_loopback_interface_enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_loopback_interface.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_port_enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_port.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_portchannel_enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_portchannel.proto
-protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_vlan_enums.proto
 protoc -I=$proto_imports --java_out=$java_output api/protobuf/openconfig/sonic_vlan.proto
--- tools/modify.sh
+++ tools/modify.sh
@@ -4,7 +4,6 @@ find .tmp -name "*.proto" | xargs sed -i 's/^package openconfig.*;$/package open
 find .tmp -name "*.proto" | xargs sed -i 's/^import "github.com.*\//import "/g'
 find .tmp -name "*.proto" | xargs sed -i 's/^import "openconfig.*\//import "/g'
 find .tmp -name "*.proto" | xargs sed -i 's/openconfig\.\w*\./openconfig./g'
-#find .tmp -name "*.proto" | xargs sed -i '/^package openconfig;$/a\option go_package = "github.com/TeamNocsys/sonicpb/api/protobuf/openconfig";'
 find .tmp -name "*.proto" | xargs sed -i '/^package openconfig;$/a\option java_package = "com.nocsys.openconfig";'
 
 find .tmp -name "*.proto" | xargs -I '{}' cp {} ../api/protobuf/openconfig
