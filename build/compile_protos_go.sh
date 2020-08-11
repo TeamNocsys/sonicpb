439e1f0bb3c3d61d6c29eaa3eefb8f9e77a9b934
sgkind <sunguanke11@mails.ucas.edu.cn>
2020-08-11 17:41:16 +0800

    编译前判断go文件夹是否存在，如果不存在则创建，如果存在则删除其中的文件

--- build/compile_protos_go.sh
+++ build/compile_protos_go.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 export GOPATH=`go env GOPATH`
+
+go_output="go"
+
+if [ ! -d  "$go_output" ];then
+mkdir "$go_output"
+else
+path="$go_output""/sonic*"
+rm -f $path 
+fi
+
 proto_imports="../protobuf/sonic:${GOPATH}/src:${GOPATH}/src/github.com/google/protobuf/src"
-protoc -I=$proto_imports --go_out=go `find ../protobuf/sonic -name "*.proto" ! -name "yext.proto" ! -name "ywrapper.proto" | xargs echo`
+protoc -I=$proto_imports --go_out=$go_output `find ../protobuf/sonic -name "*.proto" ! -name "yext.proto" ! -name "ywrapper.proto" | xargs echo`
