edf38b4c3572263484f2cd57e1ee361dcced6a6d
sgkind <sunguanke11@mails.ucas.edu.cn>
2020-08-11 17:28:36 +0800

    如果输入文件夹存在则在编译前删除

--- build/build.sh
+++ build/build.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
 export GOPATH=`go env GOPATH`
-go run ${GOPATH}/src/github.com/openconfig/ygot/proto_generator/protogenerator.go -output_dir=.tmp -exclude_modules=ietf-interfaces `find ../yang/ -path '../yang/sonic/*.yang' -o -path '../yang/sonic/third_party/ietf/*.yang' | xargs echo`
-find .tmp -name "*.proto" | xargs sed -i 's/^package openconfig.*;$/package openconfig;/g'
-find .tmp -name "*.proto" | xargs sed -i 's/^import "github.com.*\//import "/g'
-find .tmp -name "*.proto" | xargs sed -i 's/^import "openconfig.*\//import "/g'
-find .tmp -name "*.proto" | xargs sed -i 's/openconfig\.\w*\./openconfig./g'
-find .tmp -name "*.proto" | xargs sed -i '/^package openconfig;$/a\option java_package = "com.nocsys.openconfig";'
-find .tmp -name "*.proto" | xargs sed -i 's/^import "enums.proto";/import "sonic_enums.proto";/g'
-for file in `find .tmp -name "enums.proto"`
+output="tmp"
+
+if [ ! -d "$output" ];then
+rm -rf "$output"
+fi
+
+go run ${GOPATH}/src/github.com/openconfig/ygot/proto_generator/protogenerator.go -output_dir=$output -exclude_modules=ietf-interfaces `find ../yang/ -path '../yang/sonic/*.yang' -o -path '../yang/sonic/third_party/ietf/*.yang' | xargs echo`
+find $output -name "*.proto" | xargs sed -i 's/^package openconfig.*;$/package openconfig;/g'
+find $output -name "*.proto" | xargs sed -i 's/^import "github.com.*\//import "/g'
+find $output -name "*.proto" | xargs sed -i 's/^import "openconfig.*\//import "/g'
+find $output -name "*.proto" | xargs sed -i 's/openconfig\.\w*\./openconfig./g'
+find $output -name "*.proto" | xargs sed -i '/^package openconfig;$/a\option java_package = "com.nocsys.openconfig";'
+find $output -name "*.proto" | xargs sed -i 's/^import "enums.proto";/import "sonic_enums.proto";/g'
+for file in `find $output -name "enums.proto"`
 do
   newfile=`echo $file | sed s/enums.proto/sonic_enums.proto/`
   mv $file $newfile
 done
-find .tmp -name "*.proto" | xargs -i mv {} ../protobuf/sonic
+find $output -name "*.proto" | xargs -i mv {} ../protobuf/sonic
 cp ../protobuf/yext/yext.proto ../protobuf/sonic
 cp ../protobuf/ywrapper/ywrapper.proto ../protobuf/sonic
-rm -rf .tmp
+rm -rf $output
